2a095c1750d20f2ec9b2cbd94027dea51ac0d324
Nikita Fediuchin <cfnptr@gmail.com>
2022-01-29 18:07:28 +0200

    Fix image copy

--- include/mpgx/_source/image.h
+++ include/mpgx/_source/image.h
@@ -522,14 +522,14 @@ inline static MpgxResult createVkImage(
 			&bufferImageCopy);
 
 		imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-		imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+		imageMemoryBarrier.dstAccessMask = VK_ACCESS_NONE_KHR;
 		imageMemoryBarrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
 		imageMemoryBarrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
 		vkCmdPipelineBarrier(
 			transferCommandBuffer,
 			VK_PIPELINE_STAGE_TRANSFER_BIT,
-			VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+			VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
 			0,
 			0,
 			NULL,

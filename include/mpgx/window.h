477f3f25ce6fc68b1223eff8a59c1e69ded23ee9
Nikita Fediuchin <cfnptr@gmail.com>
2022-06-06 12:17:30 +0300

    Add keyboard key string conversion

--- include/mpgx/window.h
+++ include/mpgx/window.h
@@ -21,6 +21,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #define DEFAULT_WINDOW_TITLE "MPGX (v" MPGX_VERSION_STRING ")"
 #define DEFAULT_WINDOW_WIDTH 1280
@@ -2217,3 +2218,316 @@ void destroyRayTracingScene(RayTracingScene scene);
 Window getRayTracingSceneWindow(RayTracingScene scene);
 RayTracingMesh* getRayTracingSceneMeshes(RayTracingScene scene);
 size_t getRayTracingSceneMeshCount(RayTracingScene scene);
+
+/*
+ * Keyboard key name structure.
+ */
+typedef struct KeyboardKeyName
+{
+	const char* name;
+	int length;
+	KeyboardKey key;
+} KeyboardKeyName;
+
+/*
+ * Keyboard key name strings. (Sorted for binary search)
+ */
+static const KeyboardKeyName keyboardKeyNames[] =
+{
+	{ "a", 1, A_KEYBOARD_KEY },
+	{ "b", 1, B_KEYBOARD_KEY },
+	{ "c", 1, C_KEYBOARD_KEY },
+	{ "d", 1, D_KEYBOARD_KEY },
+	{ "e", 1, E_KEYBOARD_KEY },
+	{ "f", 1, F_KEYBOARD_KEY },
+	{ "g", 1, G_KEYBOARD_KEY },
+	{ "h", 1, H_KEYBOARD_KEY },
+	{ "i", 1, I_KEYBOARD_KEY },
+	{ "j", 1, J_KEYBOARD_KEY },
+	{ "k", 1, K_KEYBOARD_KEY },
+	{ "l", 1, L_KEYBOARD_KEY },
+	{ "m", 1, M_KEYBOARD_KEY },
+	{ "n", 1, N_KEYBOARD_KEY },
+	{ "o", 1, O_KEYBOARD_KEY },
+	{ "p", 1, P_KEYBOARD_KEY },
+	{ "q", 1, Q_KEYBOARD_KEY },
+	{ "r", 1, R_KEYBOARD_KEY },
+	{ "s", 1, S_KEYBOARD_KEY },
+	{ "t", 1, T_KEYBOARD_KEY },
+	{ "u", 1, U_KEYBOARD_KEY },
+	{ "v", 1, V_KEYBOARD_KEY },
+	{ "w", 1, W_KEYBOARD_KEY },
+	{ "x", 1, X_KEYBOARD_KEY },
+	{ "y", 1, Y_KEYBOARD_KEY },
+	{ "z", 1, Z_KEYBOARD_KEY },
+	{ "f1", 2, F1_KEYBOARD_KEY },
+	{ "f2", 2, F2_KEYBOARD_KEY },
+	{ "f3", 2, F3_KEYBOARD_KEY },
+	{ "f4", 2, F4_KEYBOARD_KEY },
+	{ "f5", 2, F5_KEYBOARD_KEY },
+	{ "f6", 2, F6_KEYBOARD_KEY },
+	{ "f7", 2, F7_KEYBOARD_KEY },
+	{ "f8", 2, F8_KEYBOARD_KEY },
+	{ "f9", 2, F9_KEYBOARD_KEY },
+	{ "n0", 2, N0_KEYBOARD_KEY },
+	{ "n1", 2, N1_KEYBOARD_KEY },
+	{ "n2", 2, N2_KEYBOARD_KEY },
+	{ "n3", 2, N3_KEYBOARD_KEY },
+	{ "n4", 2, N4_KEYBOARD_KEY },
+	{ "n5", 2, N5_KEYBOARD_KEY },
+	{ "n6", 2, N6_KEYBOARD_KEY },
+	{ "n7", 2, N7_KEYBOARD_KEY },
+	{ "n8", 2, N8_KEYBOARD_KEY },
+	{ "n9", 2, N9_KEYBOARD_KEY },
+	{ "up", 2, UP_KEYBOARD_KEY },
+	{ "end", 3, END_KEYBOARD_KEY },
+	{ "f10", 3, F10_KEYBOARD_KEY },
+	{ "f11", 3, F11_KEYBOARD_KEY },
+	{ "f12", 3, F12_KEYBOARD_KEY },
+	{ "f13", 3, F13_KEYBOARD_KEY },
+	{ "f14", 3, F14_KEYBOARD_KEY },
+	{ "f15", 3, F15_KEYBOARD_KEY },
+	{ "f16", 3, F16_KEYBOARD_KEY },
+	{ "f17", 3, F17_KEYBOARD_KEY },
+	{ "f18", 3, F18_KEYBOARD_KEY },
+	{ "f19", 3, F19_KEYBOARD_KEY },
+	{ "f20", 3, F20_KEYBOARD_KEY },
+	{ "f21", 3, F21_KEYBOARD_KEY },
+	{ "f22", 3, F22_KEYBOARD_KEY },
+	{ "f23", 3, F23_KEYBOARD_KEY },
+	{ "f24", 3, F24_KEYBOARD_KEY },
+	{ "f25", 3, F25_KEYBOARD_KEY },
+	{ "tab", 3, TAB_KEYBOARD_KEY },
+	{ "down", 4, DOWN_KEYBOARD_KEY },
+	{ "home", 4, HOME_KEYBOARD_KEY },
+	{ "kp_0", 4, KP_0_KEYBOARD_KEY },
+	{ "kp_1", 4, KP_1_KEYBOARD_KEY },
+	{ "kp_2", 4, KP_2_KEYBOARD_KEY },
+	{ "kp_3", 4, KP_3_KEYBOARD_KEY },
+	{ "kp_4", 4, KP_4_KEYBOARD_KEY },
+	{ "kp_5", 4, KP_5_KEYBOARD_KEY },
+	{ "kp_6", 4, KP_6_KEYBOARD_KEY },
+	{ "kp_7", 4, KP_7_KEYBOARD_KEY },
+	{ "kp_8", 4, KP_8_KEYBOARD_KEY },
+	{ "kp_9", 4, KP_9_KEYBOARD_KEY },
+	{ "left", 4, LEFT_KEYBOARD_KEY },
+	{ "menu", 4, MENU_KEYBOARD_KEY },
+	{ "comma", 5, COMMA_KEYBOARD_KEY },
+	{ "enter", 5, ENTER_KEYBOARD_KEY },
+	{ "equal", 5, EQUAL_KEYBOARD_KEY },
+	{ "minus", 5, MINUS_KEYBOARD_KEY },
+	{ "pause", 5, PAUSE_KEYBOARD_KEY },
+	{ "right", 5, RIGHT_KEYBOARD_KEY },
+	{ "slash", 5, SLASH_KEYBOARD_KEY },
+	{ "space", 5, SPACE_KEYBOARD_KEY },
+	{ "delete", 6, DELETE_KEYBOARD_KEY },
+	{ "escape", 6, ESCAPE_KEYBOARD_KEY },
+	{ "insert", 6, INSERT_KEYBOARD_KEY },
+	{ "kp_add", 6, KP_ADD_KEYBOARD_KEY },
+	{ "period", 6, PERIOD_KEYBOARD_KEY },
+	{ "page_up", 7, PAGE_UP_KEYBOARD_KEY },
+	{ "world_1", 7, WORLD_1_KEYBOARD_KEY },
+	{ "world_2", 7, WORLD_2_KEYBOARD_KEY },
+	{ "kp_enter", 8, KP_ENTER_KEYBOARD_KEY },
+	{ "kp_equal", 8, KP_EQUAL_KEYBOARD_KEY },
+	{ "left_alt", 8, LEFT_ALT_KEYBOARD_KEY },
+	{ "num_lock", 8, NUM_LOCK_KEYBOARD_KEY },
+	{ "backslash", 9, BACKSLASH_KEYBOARD_KEY },
+	{ "backspace", 9, BACKSPACE_KEYBOARD_KEY },
+	{ "caps_lock", 9, CAPS_LOCK_KEYBOARD_KEY },
+	{ "kp_divide", 9, KP_DIVIDE_KEYBOARD_KEY },
+	{ "page_down", 9, PAGE_DOWN_KEYBOARD_KEY },
+	{ "right_alt", 9, RIGHT_ALT_KEYBOARD_KEY },
+	{ "semicolon", 9, SEMICOLON_KEYBOARD_KEY },
+	{ "apostrophe", 10, APOSTROPHE_KEYBOARD_KEY },
+	{ "kp_decimal", 10, KP_DECIMAL_KEYBOARD_KEY },
+	{ "left_shift", 10, LEFT_SHIFT_KEYBOARD_KEY },
+	{ "left_super", 10, LEFT_SUPER_KEYBOARD_KEY },
+	{ "kp_multiply", 11, KP_MULTIPLY_KEYBOARD_KEY },
+	{ "kp_subtract", 11, KP_SUBTRACT_KEYBOARD_KEY },
+	{ "right_shift", 11, RIGHT_SHIFT_KEYBOARD_KEY },
+	{ "right_super", 11, RIGHT_SUPER_KEYBOARD_KEY },
+	{ "scroll_lock", 11, SCROLL_LOCK_KEYBOARD_KEY },
+	{ "grave_accent", 12, GRAVE_ACCENT_KEYBOARD_KEY },
+	{ "left_bracket", 12, LEFT_BRACKET_KEYBOARD_KEY },
+	{ "left_control", 12, LEFT_CONTROL_KEYBOARD_KEY },
+	{ "print_screen", 12, PRINT_SCREEN_KEYBOARD_KEY },
+	{ "right_bracket", 13, RIGHT_BRACKET_KEYBOARD_KEY },
+	{ "right_control", 13, RIGHT_CONTROL_KEYBOARD_KEY },
+};
+
+/*
+ * Returns 0 if keyboard key names are the same.
+ *
+ * a - pointer to the first keyboard key name.
+ * b - pointer to the second keyboard key name.
+ */
+static int compareKeyboardKeyNames(
+	const void* a, const void* b)
+{
+	const KeyboardKeyName* ak = (const KeyboardKeyName*)a;
+	const KeyboardKeyName* bk = (const KeyboardKeyName*)b;
+	int difference = ak->length - bk->length;
+	if (difference != 0) return difference;
+	return memcmp(ak->name, bk->name, ak->length * sizeof(char));
+}
+/*
+ * Get keyboard key by the name string.
+ * Returns keyborad key.
+ *
+ * name - keyboard key name string.
+ * length - name string length.
+ */
+inline static KeyboardKey stringToKeyboardKey(
+	const char* name, int length)
+{
+	assert(name);
+	assert(length > 0);
+
+	KeyboardKeyName keyboardKeyName = { name, length, };
+
+	const KeyboardKeyName* pointer = bsearch(
+		&keyboardKeyName,
+		keyboardKeyNames,
+		sizeof(keyboardKeyNames) / sizeof(KeyboardKeyName),
+		sizeof(KeyboardKeyName),
+		compareKeyboardKeyNames);
+
+	if (!pointer)
+		return UNKNOWN_KEYBOARD_KEY;
+
+	return pointer->key;
+}
+/*
+ * Get keyboard key name string by the value.
+ * Returns keyborad key name string.
+ *
+ * key - keyboard key value.
+ */
+inline static const char* keyboardKeyToString(KeyboardKey key)
+{
+	switch (key)
+	{
+	default: return "unknown";
+	case SPACE_KEYBOARD_KEY: return "space";
+	case APOSTROPHE_KEYBOARD_KEY: return "apostrophe";
+	case COMMA_KEYBOARD_KEY: return "comma";
+	case MINUS_KEYBOARD_KEY: return "minus";
+	case PERIOD_KEYBOARD_KEY: return "period";
+	case SLASH_KEYBOARD_KEY: return "slash";
+	case N0_KEYBOARD_KEY: return "n0";
+	case N1_KEYBOARD_KEY: return "n1";
+	case N2_KEYBOARD_KEY: return "n2";
+	case N3_KEYBOARD_KEY: return "n3";
+	case N4_KEYBOARD_KEY: return "n4";
+	case N5_KEYBOARD_KEY: return "n5";
+	case N6_KEYBOARD_KEY: return "n6";
+	case N7_KEYBOARD_KEY: return "n7";
+	case N8_KEYBOARD_KEY: return "n8";
+	case N9_KEYBOARD_KEY: return "n9";
+	case SEMICOLON_KEYBOARD_KEY: return "semicolon";
+	case EQUAL_KEYBOARD_KEY: return "equal";
+	case A_KEYBOARD_KEY: return "a";
+	case B_KEYBOARD_KEY: return "b";
+	case C_KEYBOARD_KEY: return "c";
+	case D_KEYBOARD_KEY: return "d";
+	case E_KEYBOARD_KEY: return "e";
+	case F_KEYBOARD_KEY: return "f";
+	case G_KEYBOARD_KEY: return "g";
+	case H_KEYBOARD_KEY: return "h";
+	case I_KEYBOARD_KEY: return "i";
+	case J_KEYBOARD_KEY: return "j";
+	case K_KEYBOARD_KEY: return "k";
+	case L_KEYBOARD_KEY: return "l";
+	case M_KEYBOARD_KEY: return "m";
+	case N_KEYBOARD_KEY: return "n";
+	case O_KEYBOARD_KEY: return "o";
+	case P_KEYBOARD_KEY: return "p";
+	case Q_KEYBOARD_KEY: return "q";
+	case R_KEYBOARD_KEY: return "r";
+	case S_KEYBOARD_KEY: return "s";
+	case T_KEYBOARD_KEY: return "t";
+	case U_KEYBOARD_KEY: return "u";
+	case V_KEYBOARD_KEY: return "v";
+	case W_KEYBOARD_KEY: return "w";
+	case X_KEYBOARD_KEY: return "x";
+	case Y_KEYBOARD_KEY: return "y";
+	case Z_KEYBOARD_KEY: return "z";
+	case LEFT_BRACKET_KEYBOARD_KEY: return "left_bracket";
+	case BACKSLASH_KEYBOARD_KEY: return "backslash";
+	case RIGHT_BRACKET_KEYBOARD_KEY: return "right_bracket";
+	case GRAVE_ACCENT_KEYBOARD_KEY: return "grave_accent";
+	case WORLD_1_KEYBOARD_KEY: return "world_1";
+	case WORLD_2_KEYBOARD_KEY: return "world_2";
+	case ESCAPE_KEYBOARD_KEY: return "escape";
+	case ENTER_KEYBOARD_KEY: return "enter";
+	case TAB_KEYBOARD_KEY: return "tab";
+	case BACKSPACE_KEYBOARD_KEY: return "backspace";
+	case INSERT_KEYBOARD_KEY: return "insert";
+	case DELETE_KEYBOARD_KEY: return "delete";
+	case RIGHT_KEYBOARD_KEY: return "right";
+	case LEFT_KEYBOARD_KEY: return "left";
+	case DOWN_KEYBOARD_KEY: return "down";
+	case UP_KEYBOARD_KEY: return "up";
+	case PAGE_UP_KEYBOARD_KEY: return "page_up";
+	case PAGE_DOWN_KEYBOARD_KEY: return "page_down";
+	case HOME_KEYBOARD_KEY: return "home";
+	case END_KEYBOARD_KEY: return "end";
+	case CAPS_LOCK_KEYBOARD_KEY: return "caps_lock";
+	case SCROLL_LOCK_KEYBOARD_KEY: return "scroll_lock";
+	case NUM_LOCK_KEYBOARD_KEY: return "num_lock";
+	case PRINT_SCREEN_KEYBOARD_KEY: return "print_screen";
+	case PAUSE_KEYBOARD_KEY: return "pause";
+	case F1_KEYBOARD_KEY: return "f1";
+	case F2_KEYBOARD_KEY: return "f2";
+	case F3_KEYBOARD_KEY: return "f3";
+	case F4_KEYBOARD_KEY: return "f4";
+	case F5_KEYBOARD_KEY: return "f5";
+	case F6_KEYBOARD_KEY: return "f6";
+	case F7_KEYBOARD_KEY: return "f7";
+	case F8_KEYBOARD_KEY: return "f8";
+	case F9_KEYBOARD_KEY: return "f9";
+	case F10_KEYBOARD_KEY: return "f10";
+	case F11_KEYBOARD_KEY: return "f11";
+	case F12_KEYBOARD_KEY: return "f12";
+	case F13_KEYBOARD_KEY: return "f13";
+	case F14_KEYBOARD_KEY: return "f14";
+	case F15_KEYBOARD_KEY: return "f15";
+	case F16_KEYBOARD_KEY: return "f16";
+	case F17_KEYBOARD_KEY: return "f17";
+	case F18_KEYBOARD_KEY: return "f18";
+	case F19_KEYBOARD_KEY: return "f19";
+	case F20_KEYBOARD_KEY: return "f20";
+	case F21_KEYBOARD_KEY: return "f21";
+	case F22_KEYBOARD_KEY: return "f22";
+	case F23_KEYBOARD_KEY: return "f23";
+	case F24_KEYBOARD_KEY: return "f24";
+	case F25_KEYBOARD_KEY: return "f25";
+	case KP_0_KEYBOARD_KEY: return "kp_0";
+	case KP_1_KEYBOARD_KEY: return "kp_1";
+	case KP_2_KEYBOARD_KEY: return "kp_2";
+	case KP_3_KEYBOARD_KEY: return "kp_3";
+	case KP_4_KEYBOARD_KEY: return "kp_4";
+	case KP_5_KEYBOARD_KEY: return "kp_5";
+	case KP_6_KEYBOARD_KEY: return "kp_6";
+	case KP_7_KEYBOARD_KEY: return "kp_7";
+	case KP_8_KEYBOARD_KEY: return "kp_8";
+	case KP_9_KEYBOARD_KEY: return "kp_9";
+	case KP_DECIMAL_KEYBOARD_KEY: return "kp_decimal";
+	case KP_DIVIDE_KEYBOARD_KEY: return "kp_divide";
+	case KP_MULTIPLY_KEYBOARD_KEY: return "kp_multiply";
+	case KP_SUBTRACT_KEYBOARD_KEY: return "kp_subtract";
+	case KP_ADD_KEYBOARD_KEY: return "kp_add";
+	case KP_ENTER_KEYBOARD_KEY: return "kp_enter";
+	case KP_EQUAL_KEYBOARD_KEY: return "kp_equal";
+	case LEFT_SHIFT_KEYBOARD_KEY: return "left_shift";
+	case LEFT_CONTROL_KEYBOARD_KEY: return "left_control";
+	case LEFT_ALT_KEYBOARD_KEY: return "left_alt";
+	case LEFT_SUPER_KEYBOARD_KEY: return "left_super";
+	case RIGHT_SHIFT_KEYBOARD_KEY: return "right_shift";
+	case RIGHT_CONTROL_KEYBOARD_KEY: return "right_control";
+	case RIGHT_ALT_KEYBOARD_KEY: return "right_alt";
+	case RIGHT_SUPER_KEYBOARD_KEY: return "right_super";
+	case MENU_KEYBOARD_KEY: return "menu";
+	}
+}
